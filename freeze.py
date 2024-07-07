6fc18a404cfaeb0e2c31d8491759cec4a094c06c
Kangddan <125175728+kangddan@users.noreply.github.com>
2024-07-07 21:40:15 +0800

    Add files via upload

---
+++ freeze.py
@@ -0,0 +1,25 @@
+import maya.cmds as cmds
+import maya.api.OpenMaya as om2
+
+def freezeAll() -> None:
+    sel = cmds.ls(sl=True, typ='transform')
+    if not sel: return
+
+    for obj in sel:
+        localMatrix = om2.MMatrix(cmds.getAttr('{}.matrix'.format(obj)))
+        offMatrix   = om2.MMatrix(cmds.getAttr('{}.offsetParentMatrix'.format(obj)))
+        cmds.setAttr('{}.offsetParentMatrix'.format(obj), localMatrix*offMatrix, typ='matrix')
+        cmds.xform(obj, m=om2.MMatrix(), ws=False)
+    
+def unfreezeAll() -> None:
+    sel = cmds.ls(sl=True, typ='transform')
+    if not sel: return
+    
+    for obj in sel:
+        localMatrix = om2.MMatrix(cmds.getAttr('{}.matrix'.format(obj)))
+        offMatrix   = om2.MMatrix(cmds.getAttr('{}.offsetParentMatrix'.format(obj)))
+        cmds.setAttr('{}.offsetParentMatrix'.format(obj), om2.MMatrix(), typ='matrix')
+        cmds.xform(obj, m=localMatrix*offMatrix, ws=False)
+        
+
+        
